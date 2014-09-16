ac5b1b6cc00242ff0283e44eca8243eca9751df3
Gordon L. Hempton <ghempton@gmail.com>
2014-09-16 06:34:19 -0700

    hasMany now accepts collectionType option

--- src/model/has_many.js
+++ src/model/has_many.js
@@ -4,17 +4,25 @@ import HasManyArray from '../collections/has_many_array';
 import isEqual from '../utils/is_equal';
 import copy from '../utils/copy';
 
+var defaults = _.defaults;
+
 export default class HasMany extends Relationship {
+  
+  constructor(name, options) {
+    defaults(options, {collectionType: HasManyArray});
+    super(name, options);
+  }
   
   defineProperty(prototype) {
     var name = this.name;
+    var field = this;
     Object.defineProperty(prototype, name, {
       enumerable: true,
       get: function() {
         var value = this._relationships[name];
         if(this.isNew && !value) {
           var content = value;
-          value = this._relationships[name] = new Coalesce.HasManyArray();
+          value = this._relationships[name] = new field.collectionType();
           value.owner = this;
           value.name = name;
           if(content) {
@@ -26,12 +34,12 @@ export default class HasMany extends Relationship {
       set: function(value) {
         var oldValue = this._relationships[name];
         if(oldValue === value) return;
-        if(value && value instanceof Coalesce.HasManyArray) {
+        if(value && value instanceof field.collectionType) {
           // XXX: this logic might not be necessary without Ember
           // need to copy since this content is being listened to
           value = copy(value);
         }
-        if(oldValue && oldValue instanceof Coalesce.HasManyArray) {
+        if(oldValue && oldValue instanceof field.collectionType) {
           oldValue.clear();
           if(value) {
             oldValue.addObjects(value);
@@ -40,7 +48,7 @@ export default class HasMany extends Relationship {
           this.hasManyWillChange(name);
           
           var content = value;
-          value = this._relationships[name] = new Coalesce.HasManyArray();
+          value = this._relationships[name] = new field.collectionType();
           value.owner = this;
           value.name = name;
           if(content) {
---
+++ test/model/has_many.coffee
@@ -0,0 +1,26 @@
+`import Model from 'coalesce/model/model'`
+`import HasManyArray from 'coalesce/collections/has_many_array'`
+`import Container from 'coalesce/container'`
+
+describe 'hasMany', ->
+  
+  it 'accepts custom collectionType option', ->
+    `class CustomArray extends HasManyArray {}`
+    
+    `class User extends Model {}`
+    User.defineSchema
+      relationships:
+        tags: {kind: 'hasMany', type: 'tag', collectionType: CustomArray}
+    User.typeKey = 'user'
+    
+    `class Tag extends Model {}`
+    Tag.typeKey = 'tag'
+    
+    user = new User()
+    expect(user.tags).to.be.an.instanceOf(CustomArray)
+    
+    
+    
+    
+    
+    
