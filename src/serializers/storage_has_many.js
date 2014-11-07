eb2de77960788f1fe782b9aad02ec60f04ceda1a
Craig Heneveld <craigheneveld@udntprty-2.local>
2014-11-07 12:45:21 -0700

    comments

--- src/serializers/storage_has_many.js
+++ src/serializers/storage_has_many.js
@@ -2,14 +2,23 @@ import isEmpty from '../utils/is_empty';
 import Serializer from './base';
 
 /**
+  The serialization of session models for storage needs has_many reguardless of 
+  a model's relationship imbedded flag.
+
   @namespace serializers
   @class StorageHasManySerializer
 */
 export default class StorageHasManySerializer extends Serializer {
 
-  // takes array of {id: 1, type_key: "post"} 
-  //  and returns an array of unloaded models
-  deserialize(serialized, opts = {}) {
+  /**
+    Turn an array of objects into an array of unloaded models
+
+    @param {Array} serialized - A array of objects, 
+      ex: [{id: 1, type_key: "post"}, {id: 2, type_key: "post"} ]
+
+    @return Array array of unloaded models
+ */
+  deserialize (serialized, opts = {}) {
     if (!serialized) return [];
 
     opts.reifyClientId = false;
@@ -21,7 +30,15 @@ export default class StorageHasManySerializer extends Serializer {
     });
   }
 
-  serialize(models, opts = {}) {
+  /**
+    Turn an array of models into an array of objects
+
+    @param {Array} models - An array of models
+
+    @return Array  A array of objects, 
+      ex: [{id: 1, type_key: "post"}, {id: 2, type_key: "post"} ]
+ */
+  serialize (models, opts = {}) {
     return models.map(function(model) {
       return {
         id: model.id,
--- src/serializers/storage_model.js
+++ src/serializers/storage_model.js
@@ -1,46 +1,73 @@
 import ModelSerializer from './model';
 
 /**
+  This class decouples the storage serialization of models from the standard 
+  application/rest serialization.  This way storage serialization is 
+  controlled internally and custom application adapters won't send side effects
+  down to session serialization.
+
+  This serializer class is generic and isn't bound to a model type like its parent
+  (the normal model serializer) is.  
+
   @namespace serializers
   @class StorageModelSerializer
 */
 export default class StorageModelSerializer extends ModelSerializer {
 
-    addMeta(serialized, model) {
-        super(serialized,model);
-        this.addProperty(serialized, model, 'typeKey', 'string');
-    }
+  /**
+    Overridden: adds typeKey as a meta property for the result serialized
+    has_many objects.  The typeKey is needed for object creation during
+    deserialization.
+  */
+  addMeta (serialized, model) {
+      super(serialized,model);
+      this.addProperty(serialized, model, 'typeKey', 'string');
+  }
 
-    deserialize(hash, opts) {
-        var model = this.createModel(hash.type_key);
+  /**
+    Overridden: Now calls this.createModel(type_key) instead of the parents 
+    createModel()
+  */
+  deserialize (hash, opts) {
+      var model = this.createModel(hash.type_key);
 
-        this.extractMeta(model, hash, opts);
-        this.extractAttributes(model, hash);
-        this.extractRelationships(model, hash);
+      this.extractMeta(model, hash, opts);
+      this.extractAttributes(model, hash);
+      this.extractRelationships(model, hash);
 
-        return model;
-    }
+      return model;
+  }
 
-    createModel(typeKey) {
-        return this.typeFor(typeKey).create();
-    }
+  /**
+    Overridden: Due to this class being generic for all model types, 
+    the serialized object contains the typeKey which infers what model to create.
+  */
+  createModel (typeKey) {
+      return this.typeFor(typeKey).create();
+  }
 
-    // overwrite the has many with one for storage
-    extractProperty(model, hash, name, type, opts) {
+  /**
+    Overridden: So that session deserialization uses our custom storage 
+    has many serailzer
+  */
+  extractProperty (model, hash, name, type, opts) {
 
-        if (type === 'has-many') {
-            type = 'storage-has-many';
-        }
-
-        return super(model, hash, name, type, opts);
-    }
-
-    // overwrite the has many with one for storage
-    addProperty(serialized, model, name, type, opts) {
       if (type === 'has-many') {
-        type = 'storage-has-many';
+          type = 'storage-has-many';
       }
 
-      return super(serialized, model, name, type, opts);
+      return super(model, hash, name, type, opts);
+  }
+
+   /**
+    Overridden: So that session serialization uses our custom storage 
+    has many serailzer
+  */
+  addProperty (serialized, model, name, type, opts) {
+    if (type === 'has-many') {
+      type = 'storage-has-many';
     }
+
+    return super(serialized, model, name, type, opts);
+  }
 }
